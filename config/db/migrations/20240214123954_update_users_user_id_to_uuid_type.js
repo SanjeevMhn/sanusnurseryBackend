cf28dcf29a2f913706e2d7778d4b43cb3d1a8bbf
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-02-15 18:34:07 +0545

    [UPDATED]: updated migration to add uuid column not replace user_id

--- config/db/migrations/20240214123954_update_users_user_id_to_uuid_type.js
+++ config/db/migrations/20240214123954_update_users_user_id_to_uuid_type.js
@@ -4,10 +4,7 @@
  */
 exports.up = function(knex) {
  return knex.schema.alterTable('users', table => {
-   table.dropColumn('user_id'),
-   table.uuid('user_uuid').defaultTo(knex.raw("uuid_generate_v4()")).notNullable(),
-   table.renameColumn('user_uuid','user_id')
-   table.primary('user_id')
+   table.uuid('user_uuid').defaultTo(knex.raw("uuid_generate_v4()")).notNullable()
  })  
 };
 
@@ -17,9 +14,6 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
   return knex.schema.alterTable('users',table => {
-    table.dropColumn('user_id'),
-    table.bigIncrements('user_intId'),
-    table.renameColumn('user_intId','user_id'),
-    table.primary('user_id')
+    table.dropColumn('user_uuid')
   })
 };
