5f7b6f236f62e4590ca6439068df74a605c3bf49
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-03-15 16:50:20 +0545

    [FIXED]: fixed get user order history on newest to oldest order

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,25 +39,13 @@ const getUserOrderHistory = async(req,res) => {
     if(!userId || userId == null || userId == ''){
       return res.status(400).json({message: 'User id not found'});
     }
-    
-    //const orderId = await knex.select('order_id').from('orders').where('user_id',userId);
-   // const order = await knex.raw(`select 
-   //                                 ord.*,
-   //                                 ord.order_date::text,
-   //                                 pd.total_amount as order_total,
-   //                                 pd.payment_status,
-   //                                 pc.payment_type 
-   //                               from orders ord 
-   //                               inner join payment_detail pd on ord.order_id = pd.order_id 
-   //                               inner join payment_category pc on pd.payment_type = pc.payment_id                                  where ord.user_id = ? 
-   //                               order by ord.order_date desc`,[userId]);
-    
+   
     const order = await knex.raw(`select 
                                     ord.*,
                                     ord.order_date::text
                                   from orders ord
                                   where ord.user_id = ?
-                                  order by ord.order_date desc`,[userId])
+                                  order by ord.created_at desc`,[userId])
     
     if(order.rows.length == 0){
       return res.status(400).json({message: 'No orders found'})
